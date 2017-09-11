3fbf5a8d14160387f7c928e33bf303727d4109e8
Jinn <jozie@mbnet.fi>
2017-09-12 00:54:25 +0300

    V5 Explosions looking like matrix
    Explosion particles use same particle code as firework particles.

--- JavaScript/00_2DFireworks/fireworks.js
+++ JavaScript/00_2DFireworks/fireworks.js
@@ -1,6 +1,7 @@
 function Firework(){
     this.firework = new Particle(random(width),height);
     this.exploded = false;
+    this.particles = [];
 
     this.update = function(){
     	// Päivitetään vain jos firework on olemassa. olisi monta eri tapaa tehdä tämä, varmasti helpompia.
@@ -12,15 +13,29 @@ function Firework(){
             if(this.firework.vel.y >= 0){
             	// Lakataan piirtämästä fireworkkia koska se on räjähtänyt
                 this.exploded = true;
+                this.explode();
             }
         }
-
-
+        for (var i = 0; i < this.particles.length; i++){
+            this.particles[i].applyForce(gravity);
+            this.particles[i].update();
+        }
     }
+    this.explode = function(){
+    	for (var i = 0; i < 100; i++){
+    		// particle luodaan siihen pisteeseen jossa firework oli
+    		var p = new Particle(this.firework.pos.x, this.firework.pos.y);
+            this.particles.push(p);
+    	}
+    }
+
     this.show = function(){
     	// Sama juttu. näytetään firework vaan jos se on olemassa.
     	if (!this.exploded){
             this.firework.show();
+        }
+        for (var i = 0; i < this.particles.length; i++){
+            this.particles[i].show();
         }
     }
 }
