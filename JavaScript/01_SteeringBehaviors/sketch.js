3c9958f4763020c2bfe21833f98331ab44cb34e1
Jinn <jozie@mbnet.fi>
2017-09-13 16:58:24 +0300

    Steering behaviors having some weird behavior
    kaikki pisteet oli vain vasemmassa ylänurkassa ilman steeringiä, toisin kuin tutoriaalissa ne oli heti tekstin muodossa. mistä johtunee? Siksi sitten kaikki koodi kerralla valmiiksi. Muiden koodeissa näytti olevan samankaltaista. Selvitä johtuuko esim selaimesta.

--- JavaScript/01_SteeringBehaviors/sketch.js
+++ JavaScript/01_SteeringBehaviors/sketch.js
@@ -1,4 +1,7 @@
+// This coding train tutorial by. Daniel Shiffman
+
 var font;
+var vehicles = [];
 
 function preload(){
     //Font not included in github project. use your own
@@ -7,24 +10,37 @@ function preload(){
 function setup(){
     createCanvas(1000,300);
     background(50);
-    textFont(font);
-    textSize(160);
-    fill(255);
-    noStroke();
-    text('shiffman', 70, 210)
+    //textFont(font);
+    //textSize(160);
+    //fill(255);
+    //noStroke();
+    //text('shiffman', 70, 210)
 
-    var points = font.textToPoints('shiffman', 70, 210);
-    console.log(points);
+    // parametrit: sijainti vasemmalta, ylhäältä ja fontin koko
+    var points = font.textToPoints('shiffman', 70, 210, 160);
+    //console.log(points);
 
     for(var i=0; i < points.length; i++){
         var pt = points[i];
-        stroke(0,255,0);
-        strokeWeight(4);
-        point(pt.x, pt.y);
+        //VANHA KOODI, tilalle tulee vehiclet
+        //pallojen väri ja koko
+        //stroke(255);
+        //strokeWeight(8);
+        //point(pt.x, pt.y);
+
+        var vehicle = new Vehicle(pt.x,pt.y);
+        vehicles.push(vehicle);
     }
 
 }
 function draw(){
-
+    background(50);
+    //miksi alussa piirtää kaikki vasempaan ylänurkkaan ja sitten vasta sieltä alkaa vetää paikalleen? :D 
+    for(var i=0; i < vehicles.length; i++){
+    	var v = vehicles[i];
+    	v.behaviors();
+    	v.update();
+    	v.show();
+    }
 
 }
