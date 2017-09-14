140b5ddc0a1084f8d05c0a027b84019b87089451
Jinn <jozie@mbnet.fi>
2017-09-14 04:44:03 +0300

    Python Mini - Leap year

---
+++ Python/minis/leapyearloop.py
@@ -0,0 +1,9 @@
+# Jinn's Python miniprograms: leap year checker
+line = input()
+while line:
+    line = int(line)
+    if line % 4 == 0 and (line % 100 != 0 or line % 400 == 0):
+        print("The year {0} is a leap year.".format(line))
+    else:
+        print("The year {0} is not a leap year.".format(line))
+    line = input()
