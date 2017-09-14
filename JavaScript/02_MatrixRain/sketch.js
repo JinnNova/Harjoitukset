75328c7cc3ff96affc0d821c44cbdb28c30e5f6c
Jinn <jozie@mbnet.fi>
2017-09-14 05:03:55 +0300

    Base for matrix rain effect
    full width canvas

---
+++ JavaScript/02_MatrixRain/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(
+        window.innerWidth,
+        window.innerHeight
+    );
+    background(10);
+}
+function draw(){
+    // draw on functio jota kutsutaan loopissa, 60 frames per second
+}
