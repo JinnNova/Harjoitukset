f94cb15ee637b7d027db84971d601251c108d5c4
Jinn <jozie@mbnet.fi>
2017-09-15 23:07:52 +0300

    V3 Changing symbol in random interval
    Not changing it every frame, but every N:th frame. Heavily commented code.

--- JavaScript/02_MatrixRain/sketch.js
+++ JavaScript/02_MatrixRain/sketch.js
@@ -25,15 +25,25 @@ function Symbol(x,y,speed){
     this.y = y;
     this.value;
     this.speed = speed;
+    // random whole number = round(random(väli))
+    // Tämä päättää kuinka usein symboli vaihtuu (every N:th frame) N = 5..20
+    this.switchInterval = round(random(5,20));
 
     // SYMBOLINHAKU-FUNKTIO
     // Käytetään matrixiin katakanaa, katakana unicode block alkaa kohdasta 0x30A0 = 12448 decimalina
     this.setToRandomSymbol = function(){
-    	// Random katakana stringiksi ja asetetaan valuemuuttujaan
-        this.value = String.fromCharCode(
-        	// lisätään 12448:iin random luku 0-96 välillä koska katakanoja on 96
-        	0x30A0 + round(random(0,96))
-        );
+    	// frameCount on P5.js variable joka pitää kirjaa framejen määrästä
+    	// % = modulo, antaa jakojäännöksen
+    	// ELI: Aina kun framecount on jaollinen luvulla 2-20 niin suoritetaan symbolinvaihto
+    	// -->> asettaa uuden symbolin siis every N:th frame!
+        if(frameCount % this.switchInterval == 0){
+            // Random katakana stringiksi ja asetetaan valuemuuttujaan
+            this.value = String.fromCharCode(
+                // lisätään 12448:iin random luku 0-96 välillä koska katakanoja on 96
+                0x30A0 + round(random(0,96))
+            );
+        }
+
     }
 
     // SYMBOLIN NÄYTTÄMIS-FUNKTIO 
@@ -43,6 +53,7 @@ function Symbol(x,y,speed){
         text(this.value, this.x, this.y);
         // Let's make it rain :D
         this.rain();
+        this.setToRandomSymbol();
     }
 
     // jos y koskettaa pohjaa niin resetataan se (ei tarvi looppeja koska draw on loop)
