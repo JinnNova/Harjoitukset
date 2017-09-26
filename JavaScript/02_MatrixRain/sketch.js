e9feb904e0400fd7d7c6622be2489dd066321b37
Jinn <jozie@mbnet.fi>
2017-09-26 23:42:55 +0300

    V6 Some streams have light first character
    Finished for now. Thanks to Emily Xie for this tutorial.

--- JavaScript/02_MatrixRain/sketch.js
+++ JavaScript/02_MatrixRain/sketch.js
@@ -10,10 +10,9 @@ function setup(){
     background(10);
 
     var x = 0;
-    var y = 0;
     for(var i = 0; i <= width / symbolSize; i++){
         var stream = new Stream();
-        stream.generateSymbols(x,y);
+        stream.generateSymbols(x,random(-1000,0));
         streams.push(stream);
         x += symbolSize;
     }
@@ -22,13 +21,13 @@ function setup(){
 
 function draw(){
     // draw on functio jota kutsutaan loopissa, 60 frames per second
-    background(10); // halutaan piirtää background uusiksi joka framessa
+    background(10,150); // halutaan piirtää background uusiksi joka framessa
     streams.forEach(function(stream){
         stream.render();
     });
 }
 
-function Symbol(x,y,speed){
+function Symbol(x,y,speed,first){
     this.x = x;
     this.y = y;
     this.value;
@@ -36,14 +35,15 @@ function Symbol(x,y,speed){
     // random whole number = round(random(väli))
     // Tämä päättää kuinka usein symboli vaihtuu (every N:th frame) N = 5..20
     this.switchInterval = round(random(5,20));
+    this.first = first;
 
     // SYMBOLINHAKU-FUNKTIO
     // Käytetään matrixiin katakanaa, katakana unicode block alkaa kohdasta 0x30A0 = 12448 decimalina
     this.setToRandomSymbol = function(){
-    	// frameCount on P5.js variable joka pitää kirjaa framejen määrästä
-    	// % = modulo, antaa jakojäännöksen
-    	// ELI: Aina kun framecount on jaollinen luvulla 2-20 niin suoritetaan symbolinvaihto
-    	// -->> asettaa uuden symbolin siis every N:th frame!
+        // frameCount on P5.js variable joka pitää kirjaa framejen määrästä
+        // % = modulo, antaa jakojäännöksen
+        // ELI: Aina kun framecount on jaollinen luvulla 2-20 niin suoritetaan symbolinvaihto
+        // -->> asettaa uuden symbolin siis every N:th frame!
         if(frameCount % this.switchInterval == 0){
             // Random katakana stringiksi ja asetetaan valuemuuttujaan
             this.value = String.fromCharCode(
@@ -77,24 +77,32 @@ function Stream(){
 
     // Streamin oma funktio symbolien generoimiseen
     this.generateSymbols = function(x,y){
+        var first = round(random(0,3)) == 1;
 
         //täytetään Streamin symbols array symboleilla
         for(var i = 0; i <= this.totalSymbols; i++){
-            symbol = new Symbol(x, y, this.speed);
+            symbol = new Symbol(x, y, this.speed, first);
             symbol.setToRandomSymbol();
             // pusketaan jokainen symboli arrayyn elämään
             this.symbols.push(symbol);
             // halutaan asettaa jokainen symboli edellisen yläpuolelle
             y -= symbolSize;
+            first = false;
         }
     }
 
     // Streamin vastuulle myös symbolien renderöinti
     this.render = function(){
-    	// FOR EACH JEE! -> koska jokainen elementti on symbol, pitää this:it vaihtaa symboliksi
+        // FOR EACH JEE! -> koska jokainen elementti on symbol, pitää this:it vaihtaa symboliksi
         this.symbols.forEach(function(symbol) {
-            //tekstin väri, vähän sinistä vihreän sekaan
-            fill(0,255,70);
+        	//tekstin väri
+            if(symbol.first){
+    	        fill(180,255,180);	
+    	    }
+            else {
+                fill(0,255,70);
+    	    }
+            
             text(symbol.value, symbol.x, symbol.y);
             // Let's make it rain :D
             symbol.rain();
