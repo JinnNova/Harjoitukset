97187f09991f73d3bad6223a06c2221ac1a6f9b7
Jinn <jozie@mbnet.fi>
2017-10-04 15:18:31 +0300

    Rotating ship (no controls)

--- JavaScript/03_AsteroidsGame/sketch.js
+++ JavaScript/03_AsteroidsGame/sketch.js
@@ -7,15 +7,25 @@ function setup(){
 function draw(){
     background(10);
     ship.render();
+    ship.turn(0.1);
 }
 
 function Ship(){
     this.pos = createVector(width/2, height/2);
     this.r = 20;
+    // P5 ei käsittele asteina vaan radiantteina joten 90 astetta on PI / 2
+    this.heading = 0;
+
     this.render = function() {
-    	translate(this.pos.x, this.pos.y);
-    	noFill();
-    	stroke(255);
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading);
+        noFill();
+        stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    }
+
+    this.turn = function(angle) {
+        this.heading += angle
+
     }
 }
