51683b39e09b723173df2884af238b52f23394fc
Jinn <jozie@mbnet.fi>
2017-10-06 06:45:49 +0300

    Asteroid shaping part 2
    cool jagged asteroids

--- JavaScript/03_AsteroidsGame/asteroid.js
+++ JavaScript/03_AsteroidsGame/asteroid.js
@@ -5,22 +5,27 @@ function Asteroid() {
     // random kokoiset asteroidit
     this.r = random(15,50);
     // jotta asteroidilla olisi 5-15 vertexiä vaihtelevasti:
-    this.total = floor(random(5,15));
+    this.total = floor(random(8,15));
+    this.offset = [];
+    for (var i = 0; i < this.total; i++){
+        this.offset[i] = random(-8,8);
+    }
 
     this.render = function() {
-    	push();
-    	stroke(255);
-    	noFill();
+        push();
+        stroke(255);
+        noFill();
         translate(this.pos.x, this.pos.y);
         //ellipse(0,0, this.r * 2);
 
         beginShape();
         for (var i = 0; i < this.total; i++) {
-        	// TWO_PI = 360 astetta, map = p5 functio
-        	// tarvitaan trigonometriaa
+            // TWO_PI = 360 astetta, map = p5 functio
+            // tarvitaan trigonometriaa
             var angle = map(i,0,this.total,0,TWO_PI);
-            var x = this.r * cos(angle);
-            var y = this.r * sin(angle);
+            var r = this.r + this.offset[i];
+            var x = r * cos(angle);
+            var y = r * sin(angle);
             vertex(x,y);
         }
         // CLOSE = P5 funktio jolla shape sulkeutuu eikä jää rakoa
