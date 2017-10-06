f0ef204b21a828e9f405b2b53eeb834c35eaf133
Jinn <jozie@mbnet.fi>
2017-10-06 06:27:16 +0300

    Asteroid shaping part 1
    instead of p5 ellipse

--- JavaScript/03_AsteroidsGame/asteroid.js
+++ JavaScript/03_AsteroidsGame/asteroid.js
@@ -9,7 +9,20 @@ function Asteroid() {
     	stroke(255);
     	noFill();
         translate(this.pos.x, this.pos.y);
-        ellipse(0,0, this.r * 2);
+        //ellipse(0,0, this.r * 2);
+
+        beginShape();
+        for (var i = 0; i < 10; i++) {
+        	// TWO_PI = 360 astetta, map = p5 functio
+        	// tarvitaan trigonometriaa
+            var angle = map(i,0,10,0,TWO_PI);
+            var x = this.r * cos(angle);
+            var y = this.r * sin(angle);
+            vertex(x,y);
+        }
+        // CLOSE = P5 funktio jolla shape sulkeutuu eikä jää rakoa
+        endShape(CLOSE);
+
         pop();
         // push ja pop koska asteroideja on paljon ja halutaan että ne ovat toisistaan riippumattomia (translate p5 juttu)
     }
