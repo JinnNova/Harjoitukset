b144057d4fef87c7504bf25f5ac9ad07512c5288
Jinn <jozie@mbnet.fi>
2017-10-20 23:47:25 +0300

    One particle and the class

--- JavaScript/05_SimpleParticleSystem/sketch.js
+++ JavaScript/05_SimpleParticleSystem/sketch.js
@@ -1,13 +1,28 @@
+let p;
+
 function setup(){
     createCanvas(1200, 800);
     //että alussa on valmiiksi tausta tumma
     background(30);
+    p = new Particle();
 
 }
 
 function draw(){
     background(10,4);
-    stroke(255);
-    strokeWeight(2);
+    p.show();
 
+}
+
+class Particle {
+    constructor(){
+        this.x = 600;
+        this.y = 600;
+    }
+
+    show(){
+        stroke(255);
+        fill(255,10);
+        ellipse(this.x,this.y,16);
+    }
 }
