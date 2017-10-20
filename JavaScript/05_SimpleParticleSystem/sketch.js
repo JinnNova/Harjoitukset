45b154dbf4fe5a800941d6df2791ce9e6707cd90
Jinn <jozie@mbnet.fi>
2017-10-20 23:56:25 +0300

    Particle array and velocity

--- JavaScript/05_SimpleParticleSystem/sketch.js
+++ JavaScript/05_SimpleParticleSystem/sketch.js
@@ -1,16 +1,20 @@
-let p;
+let particles =[];
 
 function setup(){
     createCanvas(1200, 800);
     //että alussa on valmiiksi tausta tumma
     background(30);
-    p = new Particle();
+    let p = new Particle();
+    particles.push(p);
 
 }
 
 function draw(){
     background(10,4);
-    p.show();
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].show();
+    }
 
 }
 
@@ -18,6 +22,13 @@ class Particle {
     constructor(){
         this.x = 600;
         this.y = 600;
+        this.velx = random(-1,1);
+        this.vely = random(-5,-1);
+    }
+
+    update(){
+        this.x += this.velx;
+        this.y += this.vely;
     }
 
     show(){
