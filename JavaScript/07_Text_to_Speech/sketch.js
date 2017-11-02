e65d2f9dccadc925096548c5da5b7f730d4d9dc3
Jinn <jozie@mbnet.fi>
2017-11-02 20:43:56 +0200

    Finally working! speechlib was an old version. Duh.

--- JavaScript/07_Text_to_Speech/sketch.js
+++ JavaScript/07_Text_to_Speech/sketch.js
@@ -1,8 +1,21 @@
 // needs p5.speech.js library
 // needs p5 library
 
-function setup(){
-    noCanvas();
+// finally works, i just had wrong version of the speech lib. duh.
+let speechObj;
 
+function setup() {
+  noCanvas();
+
+  speechObj = new p5.Speech(); // speech synthesis object
+  speechObj.onLoad = voiceReady;
+
+    function voiceReady() {
+        //console.log('voice ready');
+        // list voice options
+        console.log(speechObj.voices);
+        // (Windows 7) + Chrome = 20 voice options.
     }
+
+  speechObj.speak('Coding Train');
 }
